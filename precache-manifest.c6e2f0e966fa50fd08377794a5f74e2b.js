9b5148b9efb643e98198027499c1d4a34afe87a1
keremulusoy <kerem.ulusoy@fireflyon.com>
2023-03-05 21:09:10 +0300

    Updates

---
+++ precache-manifest.c6e2f0e966fa50fd08377794a5f74e2b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "756a187501a18b82d277",
+    "url": "/qa-react-project/static/css/main.1d344b44.chunk.css"
+  },
+  {
+    "revision": "756a187501a18b82d277",
+    "url": "/qa-react-project/static/js/main.0a9a4508.chunk.js"
+  },
+  {
+    "revision": "ca19ce932d50c87ce06e",
+    "url": "/qa-react-project/static/js/runtime~main.fbba6d49.js"
+  },
+  {
+    "revision": "71aaa8d24fea379ad5a6",
+    "url": "/qa-react-project/static/css/2.db226f16.chunk.css"
+  },
+  {
+    "revision": "71aaa8d24fea379ad5a6",
+    "url": "/qa-react-project/static/js/2.2b788dca.chunk.js"
+  },
+  {
+    "revision": "629666658164b43f59b6",
+    "url": "/qa-react-project/static/js/3.45bd1f93.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/qa-react-project/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c08d00cac4f2edb5ade360a193fcd08e",
+    "url": "/qa-react-project/static/media/primeicons.c08d00ca.eot"
+  },
+  {
+    "revision": "c0929a1e055f2ceb5c889501c24a24e1",
+    "url": "/qa-react-project/static/media/primeicons.c0929a1e.woff2"
+  },
+  {
+    "revision": "062070972fbad0f4fbb3505e6d013a39",
+    "url": "/qa-react-project/static/media/primeicons.06207097.woff"
+  },
+  {
+    "revision": "acb19296cb26b15dcf42e13c603217f2",
+    "url": "/qa-react-project/static/media/primeicons.acb19296.ttf"
+  },
+  {
+    "revision": "1d854e3ab2c37173804a0d1d1af229ee",
+    "url": "/qa-react-project/static/media/primeicons.1d854e3a.svg"
+  },
+  {
+    "revision": "4a652321016ea21ee9e19e45a99ed817",
+    "url": "/qa-react-project/index.html"
+  }
+];
